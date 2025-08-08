2d095454839cbd17e7ad1d8c241400658de3ead2
Gabriel Duque Schiffner <76018843+gabrielduqueschiffner@users.noreply.github.com>
2025-08-08 14:37:45 -0300

    cria funcao cria_area_plantio

--- main.js
+++ main.js
@@ -4,46 +4,41 @@
 
 let ambiente = {
     temperatura: 25,    // ºC
-    umidade_ar:   60,    // %
+    umidade_ar: 60,    // %
 }
 
- let unidade_plantio ={    //esse é o solo 
-    estado ,  // se está limpo, com pedra, com erva daninha
+/* let unidade_plantio = {    //esse é o solo 
+    estado,  // se está limpo, com pedra, com erva daninha
     umidade,   // se está com agua ou não  
     preparo,    // possibilita o plantio
     fertilização  //aumenta a velocidade de crescimento
-    
-
-} 
 
 
+} */
 
-//criando o elemento pai (area de plantio)
-let area_plantio = document.querySelector('.area-plantio');
+//funcao que cria a area de plantio
+function cria_area_plantio() {
+    const nova_area = document.createElement('div');
+    nova_area.classList.add('area-plantio');
+    return nova_area;
+}
 
-
-cria_area_plantio();
-cria_unidades_plantio();
 
 
 // funcao que cria as unidades de plantio de acordo com a quantidade desejada
-function cria_unidades_plantio(){
-    let quantidade_unidade_plantio = 144;
+function cria_unidades_plantio(area_plantio) {
+    const quantidade_unidade_plantio = 144;
 
-    for(let i = 0; i < quantidade_unidade_plantio ; i++){
-    let unidade_plantio = document.createElement('div');
-    unidade_plantio.classList.add('unidade-plantio');
+    for (let i = 0; i < quantidade_unidade_plantio; i++) {
+        const unidade_plantio = document.createElement('div');
+        unidade_plantio.classList.add('unidade-plantio');
 
-    area_plantio.appendChild(unidade_plantio);
+        area_plantio.appendChild(unidade_plantio);
     }
 
-}    
-
-
-//funcao que cria a area de plantio
-
-function cria_area_plantio(){
-let nova_area_plantio = document.createElement('div');
-nova_area_plantio.classList.add('area-plantio');
- return nova_area_plantio;
 }
+
+//
+  const areaPlantio = cria_area_plantio();
+  document.body.appendChild(areaPlantio);
+  cria_unidades_plantio(areaPlantio);
